0c8cf2945e5256bcab7130f7143c8b2e79e3adc8
Natsurii <40201730+Natsurii@users.noreply.github.com>
2019-05-25 21:47:22 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import threading
+import tweepy
+import facebook
+import random
+import logging
+import schedule
+import time
+import markovify
+from PIL import Image, ImageFont, ImageDraw, ImageOps
+from urllib.request import urlopen
+import urllib.request
+from bs4 import BeautifulSoup
+import requests
+from io import BytesIO
+import os
+
+
+def chainmaker():
+	with open("headlines.txt",encoding='utf-8-sig') as f:
+		text = f.read()
+	text_model = markovify.NewlineText(text)
+	print(text_model.make_short_sentence(100,tries=100))
+
+def fbpost():
+	logging.debug('Starting Facebook Thread')
+
+	fb_token = os.environ['TOKEN_PAINTMIN']
+	content = chainmaker() 
+
+	graph = facebook.GraphAPI(access_token=fb_token, version="3.1")
+
+	post = graph.put_photo(image=open('white.png',"rb"),
+                message=content)
+
+	graph.put_object(parent_object=post['post_id'], connection_name='comments',
+                  message='Please hit the mf like button.\n Disclaimer: This is computer generated content. Any headlines that con-incide to real events are purely coincidental.')
+	logging.debug('=====================SUCCESS POSTING FB, Exiting....=====================')
+
+fbpost()
+schedule.every(10).minutes.do(fbpost)
---
+++ run.py
@@ -0,0 +1,55 @@
+
+import threading
+import tweepy
+import facebook
+import random
+import logging
+import schedule
+import time
+from PIL import Image, ImageFont, ImageDraw, ImageOps
+from urllib.request import urlopen
+import urllib.request
+from bs4 import BeautifulSoup
+import requests
+from io import BytesIO
+hl=[]
+text = open('headlines.txt','a+',encoding='utf-8-sig')
+def get_art(page):
+	html = urlopen(f'https://www.abante.com.ph/category/ent{page}')
+	soup = BeautifulSoup(html, 'html.parser')
+	art = soup.find_all('a',class_='post-title post-url')
+	headlines =[]
+
+	for i in art:
+		val = (str(i.get_text()+'\n'))
+		hl.append(val)
+get_art(page='/')
+
+for i in range(2,50):
+	ref =f'/page/{i}'
+	get_art(page=ref)
+	print(f'done page{i}')
+
+
+def Remove(duplicate): 
+	final_list = [] 
+	for num in duplicate: 
+		if num not in final_list: 
+			final_list.append(num) 
+	return final_list 
+# Driver Code 
+
+
+for i in Remove(hl):
+	print(f'writing article: {i} to the file.')
+	text.write(i)
+text.close()
+'''
+# Driver Code 
+print(hl)
+dupe = Remove(hl)
+for i in dupe:
+	print(f'writing article: {i}')
+	text.write(i)
+
+'''

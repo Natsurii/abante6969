c65d1aab3979923a988558a4554a02ba8d535941
Natsurii <40201730+Natsurii@users.noreply.github.com>
2019-12-14 13:54:16 +0800

    fix words

--- main.py
+++ main.py
@@ -32,7 +32,8 @@ def headline_factory():
 #	sg = markovify.NewlineText(text2)
 #	model = markovify.combine([sg, text_model1], [1.5, 0.2])
 	initword = random.choice(['Pasko','pasko','Christmas','christmas','New year','new year', 'regalo', 'bagong taon', 'paputok', 'pailaw', 'fireworks', 'firecracker', 'aguinaldo', 'bonus'])
-	content = text_model1.make_short_sentence(100, init_state = initword, tries=100)
+#	content = text_model1.make_short_sentence(100, tries=100)
+	content = text_model1.make_sentence_with_words(initword, tries=50)
 	logging.info(f'Headline created! \n {content}')
 	return content
 
